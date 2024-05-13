f4c0cacb9406d010f1701f65035b168ddd24c524
elevenqtwo <elevenqtwo@gmail.com>
2024-05-13 12:09:38 +0300

    chat history feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import langchain_community
 import telebot
 from dotenv import load_dotenv
 from langchain.chains.retrieval_qa.base import RetrievalQA
@@ -56,21 +55,6 @@ def create_telegram_bot(bot_token):
 #    response = call_gpt4_api(prompt)
 #    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-
-def call_gpt4_api(prompt):
-    client = OpenAI(api_key=load_api_credentials()[0])
-    try:
-        response = client.completions.create(
-            model='gpt-3.5-turbo-instruct',
-            prompt=prompt,
-        )
-
-        return response.choices[0].text.strip()
-    except Exception as e:
-        logging.error(f"Error calling GPT-4 API: {e}")
-        return "Sorry, I'm unable to answer that question."
-
-
 def load_training_data():
     with open('training_data.txt', 'r') as file:
         contents = file.read()
@@ -94,14 +78,13 @@ def main():
 
     vectorstore = create_embeddings(data)
 
-    chat = ChatOpenAI(verbose=True, temperature=0.5, model_name="gpt-3.5-turbo")
+    chat = ChatOpenAI(verbose=True, temperature=0, model_name="gpt-3.5-turbo")
     qa = RetrievalQA.from_chain_type(
         llm=chat, chain_type="stuff", retriever=vectorstore.as_retriever(),
     )
 
     bot = create_telegram_bot(telegram_bot_token)
 
-    client = OpenAI()
     #completion = client.chat.completions.create(
     #    model="gpt-3.5-turbo",
     #    messages=[{"role": "system",
@@ -116,12 +99,26 @@ def main():
     #related_words = ['latoken', 'hackaton']
     #@bot.message_handler(func=lambda message: any(word.lower() in message.text.lower() for word in related_words))
 
+    chat_history = []
+
     @bot.message_handler(func=lambda message: True)
     def echo_hackaton_related(message):
-        res = qa.invoke(message.text)
-        print(res)
-        if 'result' in res and res['result']:
-            bot.reply_to(message, res['result'])
+        #query = ("Внимательно ознакомься с данной тебе информацией, и ответь на следующий вопрос, основываясь на ней. "
+        #         "В своем ответе не упоминай, что у тебя есть guidelines и регламент, по которому ты будешь отвечать "
+        #         "(в ответе ты не должен использовать выражения, такие как \"Исходя из представленной "
+        #         "информации...\", \"Согласно предоставленной информации\", \"В предоставленных мне данных не указаны "
+        #         "подробности вашего вопроса..\". Если возникает случай, в котором тебе хотелось бы использовать "
+        #         "подобные предложения, просто передавай сообщение с тем же смыслом, но НЕ ссылаясь на "
+        #         "предоставленные тебе данные. повторю, НЕ ссылайся на твои источники"
+        #         "Вопрос на который нужно ответить: ")
+        response = qa.invoke({"query": message.text, "chat_history": chat_history})
+        print(response)
+
+        history = (response["query"], response['result'])
+        chat_history.append(history)
+
+        if 'result' in response and response['result']:
+            bot.reply_to(message, response['result'])
         else:
             bot.reply_to(message, "Я не могу ответить на этот вопрос..")
 
